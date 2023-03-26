b7a48da1f241394912d9c6e946bd9ddf0152d8ac
Christopher-Deo <chris@christopherdeo.com>
2023-03-26 18:39:36 -0500

    additonal notes exercises

---
+++ Grades.java
@@ -0,0 +1,8 @@
+public class Grades {
+    public static void main(String[] args) {
+        char potions = 'A';
+        char arithmacy = 'B';
+        char charms = 'C';
+        System.out.println("Your grades are: Potions: " + potions + " Arithmacy: "+arithmacy+" Charms: "+charms+".");
+    }
+}
